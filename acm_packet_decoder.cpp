da5d46931df3614f8d49bc6f885a6e10c78991ef
nu774 <honeycomb77@gmail.com>
2016-04-19 20:04:56 +0900

    support LPCM

--- acm_packet_decoder.cpp
+++ acm_packet_decoder.cpp
@@ -6,6 +6,7 @@
 #include <mmreg.h>
 #include <msacm.h>
 #include "../SDK/foobar2000.h"
+#include "Helpers.h"
 
 namespace {
     void acm_stream_close(HACMSTREAM h) { acmStreamClose(h, 0); }
@@ -14,14 +15,19 @@ namespace {
 class acm_packet_decoder: public packet_decoder {
     WORD                          m_wFormatTag;
     std::string                   m_codec_name;
-    WAVEFORMATEX                  m_lpcm_format;
+    WAVEFORMATEX                  m_ofmt;
+    DWORD                         m_channel_mask;        
     std::shared_ptr<HACMSTREAM__> m_stream;
     std::vector<BYTE>             m_sample_buffer;
 public:
     acm_packet_decoder()
-        : m_wFormatTag(0)
+        : m_wFormatTag(0), m_channel_mask(0)
     {
-        memset(&m_lpcm_format, 0, sizeof m_lpcm_format);
+        memset(&m_ofmt, 0, sizeof m_ofmt);
+    }
+    bool is_lpcm()
+    {
+        return m_wFormatTag == 1 || m_wFormatTag == 3;
     }
     static bool g_is_our_setup(const GUID &p_owner,  t_size p_param1,
                                const void *p_param2, t_size p_param2size)
@@ -31,14 +37,26 @@ public:
             && p_param2size == sizeof(matroska_setup)
             && std::strncmp(setup->codec_id, "A_MS/ACM", 8) == 0;
     }
-    void open(const WAVEFORMATEX *wfx)
+    bool open_lpcm(const WAVEFORMATEX *wfx)
     {
-        m_wFormatTag                 = wfx->wFormatTag;
-        m_lpcm_format.wFormatTag     = WAVE_FORMAT_PCM;
-        m_lpcm_format.nChannels      = wfx->nChannels;
-        m_lpcm_format.nSamplesPerSec = wfx->nSamplesPerSec;
+        if (is_lpcm())
+            return true;
+        else if (wfx->wFormatTag == WAVE_FORMAT_EXTENSIBLE) {
+            auto wfxe = reinterpret_cast<const WAVEFORMATEXTENSIBLE *>(wfx);
+            m_channel_mask = wfxe->dwChannelMask;
+            if (wfxe->SubFormat == KSDATAFORMAT_SUBTYPE_PCM
+             || wfxe->SubFormat == KSDATAFORMAT_SUBTYPE_IEEE_FLOAT) {
+                m_wFormatTag = wfxe->SubFormat.Data1;
+                return true;
+            }
+        }
+        return false;
+    }
+    void open_acm(const WAVEFORMATEX *wfx)
+    {
+        m_ofmt.wFormatTag = WAVE_FORMAT_PCM;
         if (acmFormatSuggest(0, const_cast<WAVEFORMATEX *>(wfx),
-                             &m_lpcm_format, sizeof m_lpcm_format,
+                             &m_ofmt, sizeof m_ofmt,
                              ACM_FORMATSUGGESTF_NCHANNELS |
                              ACM_FORMATSUGGESTF_NSAMPLESPERSEC |
                              ACM_FORMATSUGGESTF_WFORMATTAG))
@@ -53,9 +71,15 @@ public:
 
         HACMSTREAM has;
         if (acmStreamOpen(&has, 0, const_cast<WAVEFORMATEX *>(wfx),
-                          &m_lpcm_format, 0, 0, 0, 0))
+                          &m_ofmt, 0, 0, 0, 0))
             throw std::runtime_error("acmStreamOpen() failed");
         m_stream = std::shared_ptr<HACMSTREAM__>(has, acm_stream_close);
+    }
+    void open(const WAVEFORMATEX *wfx)
+    {
+        m_wFormatTag = wfx->wFormatTag;
+        m_ofmt = *wfx;
+        if (!open_lpcm(wfx)) open_acm(wfx);
     }
     void open(const GUID &p_owner, bool p_decode, t_size p_param1,
               const void *p_param2, t_size p_param2size,
@@ -64,15 +88,59 @@ public:
         auto setup = static_cast<const matroska_setup *>(p_param2);
         open(static_cast<const WAVEFORMATEX *>(setup->codec_private));
     }
-    void get_info(file_info &p_info)
+    void get_info_lpcm(file_info &p_info)
+    {
+        if (m_wFormatTag == 3)
+            p_info.info_set("codec", "PCM (floating point)");
+        else
+            p_info.info_set("codec", "PCM");
+        p_info.info_set("encoding", "lossless");
+        p_info.info_set_int("bitspersample", m_ofmt.wBitsPerSample);
+    }
+    void get_info_acm(file_info &p_info)
     {
         p_info.info_set("codec",    m_codec_name.c_str());
         p_info.info_set("encoding", "lossy");
-        p_info.info_set_int("samplerate", m_lpcm_format.nSamplesPerSec);
-        p_info.info_set_int("channels",   m_lpcm_format.nChannels);
     }
-    void decode(const void *p_buffer, t_size p_bytes, audio_chunk &p_chunk,
-                abort_callback &p_abort)
+    void get_info(file_info &p_info)
+    {
+        if (is_lpcm())
+            get_info_lpcm(p_info);
+        else
+            get_info_acm(p_info);
+
+        p_info.info_set_int("samplerate", m_ofmt.nSamplesPerSec);
+        if (!m_channel_mask)
+            p_info.info_set_int("channels",   m_ofmt.nChannels);
+        else {
+            auto s = Helpers::describe_channels(m_channel_mask);
+            p_info.info_set("channels", s.c_str());
+        }
+    }
+    void decode_lpcm(const void *p_buffer, t_size p_bytes,
+                             audio_chunk &p_chunk, abort_callback &p_abort)
+    {
+        unsigned chanmask = m_channel_mask;
+        if (!chanmask)
+            chanmask = audio_chunk::g_guess_channel_config(m_ofmt.nChannels);
+        if (m_wFormatTag == 1)
+            p_chunk.set_data_fixedpoint(p_buffer,
+                                        p_bytes,
+                                        m_ofmt.nSamplesPerSec,
+                                        m_ofmt.nChannels,
+                                        m_ofmt.wBitsPerSample,
+                                        chanmask);
+        else
+            p_chunk.set_data_floatingpoint_ex(p_buffer,
+                                              p_bytes,
+                                              m_ofmt.nSamplesPerSec,
+                                              m_ofmt.nChannels,
+                                              m_ofmt.wBitsPerSample,
+                                              audio_chunk::FLAG_SIGNED,
+                                              chanmask);
+    }
+    void decode_acm(const void *p_buffer, t_size p_bytes,
+                            audio_chunk &p_chunk, abort_callback &p_abort)
     {
         DWORD num_obytes;
         if (acmStreamSize(m_stream.get(), p_bytes, &num_obytes,
@@ -94,13 +162,23 @@ public:
         if (acmStreamUnprepareHeader(m_stream.get(), &header, 0))
             throw std::runtime_error("acmStreamUnprepareHeader() failed");
 
-        unsigned n = m_lpcm_format.nChannels;
+        unsigned chanmask = m_channel_mask;
+        if (!chanmask)
+            chanmask = audio_chunk::g_guess_channel_config(m_ofmt.nChannels);
         p_chunk.set_data_fixedpoint(m_sample_buffer.data(),
                                     header.cbDstLengthUsed,
-                                    m_lpcm_format.nSamplesPerSec,
-                                    m_lpcm_format.nChannels,
-                                    m_lpcm_format.wBitsPerSample,
-                                    audio_chunk::g_guess_channel_config(n));
+                                    m_ofmt.nSamplesPerSec,
+                                    m_ofmt.nChannels,
+                                    m_ofmt.wBitsPerSample,
+                                    chanmask);
+    }
+    void decode(const void *p_buffer, t_size p_bytes, audio_chunk &p_chunk,
+                abort_callback &p_abort)
+    {
+        if (is_lpcm())
+            decode_lpcm(p_buffer, p_bytes, p_chunk, p_abort);
+        else
+            decode_acm(p_buffer, p_bytes, p_chunk, p_abort);
     }
     t_size set_stream_property(const GUID &p_type, t_size p_param1,
                                const void *p_param2, t_size p_param2size)
@@ -133,7 +211,7 @@ static packet_decoder_factory_t<acm_packet_decoder>
 
 DECLARE_COMPONENT_VERSION(
     "ACM Packet Decoder",
-    "0.0.1",
-    "ACM Packet Decoder, Copyright (c) 2014 nu774"
+    "0.0.2",
+    "ACM Packet Decoder, Copyright (c) 2016 nu774"
 );
 VALIDATE_COMPONENT_FILENAME("foo_acm_packet_decoder.dll");
